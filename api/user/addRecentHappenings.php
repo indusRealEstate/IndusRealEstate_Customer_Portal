6ec0d1b3e2ab07215d7d3206c9c97b6c58302269
indusRealEstate <webtech@indusre.ae>
2023-01-09 12:49:48 +0400

    feat:

---
+++ api/user/addRecentHappenings.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type: application/json; charset=UTF-8");
+
+include "../dBase.php";
+$dbase = new dBase();
+$tcon = $dbase->con;
+
+$postdata = file_get_contents("php://input");
+$decodedData = json_decode($postdata, true);
+
+if ($postdata != null) {
+    $stmt = $dbase->execute("INSERT INTO `user_recent_happenings`(`user_id`, `content`, `date`) VALUES ('{$decodedData["user_id"]}','{$decodedData["content"]}','{$decodedData["date"]}')");
+
+}
+
+
+?>
