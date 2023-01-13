f8289e747e3a585d19cc1e635fac7854eb18b929
indusRealEstate <webtech@indusre.ae>
2023-01-13 12:25:22 +0400

    refactor: :recycle: added tenant registration form.

--- src/app/components/material_dropdown/dropdown.component.ts
+++ src/app/components/material_dropdown/dropdown.component.ts
@@ -1,126 +1,133 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { User } from '../../../../models/user/user.model';
-
+import { Component, OnInit, Renderer2 } from "@angular/core";
+import { User } from "../../../../models/user/user.model";
 
 /**
  * @title Basic menu
  */
 @Component({
-    selector: 'dropdown-btn',
-    templateUrl: './dropdown.component.html',
-    styleUrls: ['./dropdown.component.scss']
+  selector: "dropdown-btn",
+  templateUrl: "./dropdown.component.html",
+  styleUrls: ["./dropdown.component.scss"],
 })
 export class DropdownMaterial implements OnInit {
+  enteredButton = false;
+  isMatMenuOpen = false;
+  isMatMenu2Open = false;
+  prevButtonTrigger;
+  user: User;
+  isLandlord: boolean = false;
+  isTenant: boolean = false;
 
-    enteredButton = false;
-    isMatMenuOpen = false;
-    isMatMenu2Open = false;
-    prevButtonTrigger;
-    user: User;
+  constructor() {
+    var userData = localStorage.getItem("currentUser");
+    var user = JSON.parse(userData);
 
-    isMobileMenu() {
-        if ($(window).width() > 991) {
-            return false;
-        }
-        return true;
-    };
-
-    isPCMenu() {
-        if ($(window).width() <= 991) {
-            return false;
-        }
-        return true;
+    if (user[0]["auth_type"] == "landlord") {
+      this.isLandlord = true;
+      this.isTenant = false;
+    } else if (user[0]["auth_type"] == "tenant") {
+      this.isTenant = true;
+      this.isLandlord = false;
     }
+  }
 
-
-
-    ngOnInit() {
-        this.getUserDataFromLocal();
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
     }
+    return true;
+  }
 
-    menuenter() {
-        this.isMatMenuOpen = true;
-        if (this.isMatMenu2Open) {
-            this.isMatMenu2Open = false;
-        }
+  isPCMenu() {
+    if ($(window).width() <= 991) {
+      return false;
     }
+    return true;
+  }
 
-    menuLeave(trigger, button) {
-        setTimeout(() => {
-            if (!this.isMatMenu2Open && !this.enteredButton) {
-                this.isMatMenuOpen = false;
-                trigger.closeMenu();
-            } else {
-                this.isMatMenuOpen = false;
-            }
-        }, 80)
+  ngOnInit() {
+    this.getUserDataFromLocal();
+  }
+
+  menuenter() {
+    this.isMatMenuOpen = true;
+    if (this.isMatMenu2Open) {
+      this.isMatMenu2Open = false;
     }
+  }
 
-    menu2enter() {
-        this.isMatMenu2Open = true;
-    }
+  menuLeave(trigger, button) {
+    setTimeout(() => {
+      if (!this.isMatMenu2Open && !this.enteredButton) {
+        this.isMatMenuOpen = false;
+        trigger.closeMenu();
+      } else {
+        this.isMatMenuOpen = false;
+      }
+    }, 80);
+  }
 
-    menu2Leave(trigger1, trigger2, button) {
-        setTimeout(() => {
-            if (this.isMatMenu2Open) {
-                trigger1.closeMenu();
-                this.isMatMenuOpen = false;
-                this.isMatMenu2Open = false;
-                this.enteredButton = false;
-            } else {
-                this.isMatMenu2Open = false;
-                trigger2.closeMenu();
-            }
-        }, 100)
-    }
+  menu2enter() {
+    this.isMatMenu2Open = true;
+  }
 
-    buttonEnter(trigger) {
-        setTimeout(() => {
-            if (this.prevButtonTrigger && this.prevButtonTrigger != trigger) {
-                this.prevButtonTrigger.closeMenu();
-                this.prevButtonTrigger = trigger;
-                this.isMatMenuOpen = false;
-                this.isMatMenu2Open = false;
-                trigger.openMenu();
-            }
-            else if (!this.isMatMenuOpen) {
-                this.enteredButton = true;
-                this.prevButtonTrigger = trigger
-                trigger.openMenu();
-            }
-            else {
-                this.enteredButton = true;
-                this.prevButtonTrigger = trigger
-            }
-        })
-    }
+  menu2Leave(trigger1, trigger2, button) {
+    setTimeout(() => {
+      if (this.isMatMenu2Open) {
+        trigger1.closeMenu();
+        this.isMatMenuOpen = false;
+        this.isMatMenu2Open = false;
+        this.enteredButton = false;
+      } else {
+        this.isMatMenu2Open = false;
+        trigger2.closeMenu();
+      }
+    }, 100);
+  }
 
-    buttonLeave(trigger, button) {
-        setTimeout(() => {
-            if (this.enteredButton && !this.isMatMenuOpen) {
-                trigger.closeMenu();
-            } if (!this.isMatMenuOpen) {
-                trigger.closeMenu();
-            } else {
-                this.enteredButton = false;
-            }
-        }, 100)
-    }
-    getUserDataFromLocal() {
-        var data = localStorage.getItem('currentUser');
-        var user = JSON.parse(data);
+  buttonEnter(trigger) {
+    setTimeout(() => {
+      if (this.prevButtonTrigger && this.prevButtonTrigger != trigger) {
+        this.prevButtonTrigger.closeMenu();
+        this.prevButtonTrigger = trigger;
+        this.isMatMenuOpen = false;
+        this.isMatMenu2Open = false;
+        trigger.openMenu();
+      } else if (!this.isMatMenuOpen) {
+        this.enteredButton = true;
+        this.prevButtonTrigger = trigger;
+        trigger.openMenu();
+      } else {
+        this.enteredButton = true;
+        this.prevButtonTrigger = trigger;
+      }
+    });
+  }
 
-        this.user = new User(
-            user[0]["id"],
-            user[0]["auth_type"],
-            user[0]["username"],
-            user[0]["firstname"],
-            user[0]["lastname"],
-            user[0]["password"],
-            user[0]["token"],
-        )
-    }
+  buttonLeave(trigger, button) {
+    setTimeout(() => {
+      if (this.enteredButton && !this.isMatMenuOpen) {
+        trigger.closeMenu();
+      }
+      if (!this.isMatMenuOpen) {
+        trigger.closeMenu();
+      } else {
+        this.enteredButton = false;
+      }
+    }, 100);
+  }
+  getUserDataFromLocal() {
+    var data = localStorage.getItem("currentUser");
+    var user = JSON.parse(data);
 
+    this.user = new User(
+      user[0]["id"],
+      user[0]["auth_type"],
+      user[0]["username"],
+      user[0]["firstname"],
+      user[0]["lastname"],
+      user[0]["password"],
+      user[0]["token"]
+    );
+  }
 }
-
-
---
+++ src/app/landlord_register_form/landlord_form.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { MatTableDataSource } from "@angular/material/table";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ApiService } from "app/services/api.service";
+import { AuthenticationService } from "app/services/authentication.service";
+import { UserService } from "app/services/user.service";
+import * as CryptoJS from "crypto-js";
+import { first } from "rxjs";
+
+@Component({
+  selector: "landlord-register-form",
+  templateUrl: "./landlord_form.component.html",
+  styleUrls: ["./landlord_form.component.scss"],
+})
+export class LandlordFormComponent implements OnInit {
+  secretKey = "fiUIXSU3ZHVtMZADtUaIGxLVUKZAjjlf";
+  registerForm: FormGroup;
+  auth_type: any = "";
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.route.queryParams.subscribe((e) => {
+      var encFormData = e["id"].split("*")[0];
+      var encAuthType = e["id"].split("*")[1];
+
+      var formData = this.decrypt(encFormData);
+      var authTypeData = this.decrypt(encAuthType);
+
+      if (
+        formData == "decryption-failed" ||
+        authTypeData == "decryption-failed"
+      ) {
+        router.navigateByUrl("/register");
+      } else {
+        var rawJson = JSON.parse(formData);
+
+        this.registerForm = this.formBuilder.group({
+          firstname: [rawJson["firstname"]],
+          lastname: [rawJson["lastname"]],
+          username: [rawJson["username"]],
+          password: [rawJson["password"]],
+        });
+
+        this.auth_type = authTypeData;
+      }
+    });
+  }
+
+  ngOnInit() {
+    console.log(this.registerForm.value);
+    console.log(this.auth_type);
+  }
+
+  // this.loading = true;
+  onSubmit() {
+    this.userService
+      .register(this.registerForm.value, this.auth_type)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          // this.alertService.success("Registration successful", true);
+          this.router.navigate(["/login"]);
+        },
+        (error) => {
+          // this.alertService.error(error);
+          console.log(error);
+          // this.loading = false;
+        }
+      );
+  }
+
+  decrypt(textToDecrypt: string) {
+    try {
+      return CryptoJS.AES.decrypt(
+        textToDecrypt,
+        this.secretKey.trim()
+      ).toString(CryptoJS.enc.Utf8);
+    } catch (error) {
+      return "decryption-failed";
+    }
+  }
+
+  readFile(event: any) {
+    var file = event.target.files[0];
+
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onloadend = (e) => {
+      var buffer = e.target.result;
+      var blob = new Blob([buffer], { type: "image/jpeg" });
+
+      console.log(blob);
+
+      // this.apiService.downloadFile(blob, "profile-img", "jpg");
+    };
+  }
+}
