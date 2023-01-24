f0f375a6e8bd635e769d0e55ac14efed4956b7c6
indusRealEstate <webtech@indusre.ae>
2023-01-24 09:07:19 +0400

    feat: :art: add formbuilder in add property form and other minor ui fixes.

--- src/app/add-property-form/add_property_from.ts
+++ src/app/add-property-form/add_property_from.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ApiService } from "app/services/api.service";
 import { AuthenticationService } from "app/services/authentication.service";
@@ -12,12 +13,16 @@ export class AddPropertyForm implements OnInit {
   isUserSignedIn: boolean = false;
   textareaValue: any = "hello";
   propertyState: "sale" | "rent" = "rent";
+  addPropertyForm: FormGroup;
 
   marketingSocialMedia: boolean = false;
   marketingBoards: boolean = false;
   marketingOthers: boolean = false;
 
+  uploadedDocName: string = "No attachments found";
+
   constructor(
+    private formBuilder: FormBuilder,
     private apiService: ApiService,
     private router: Router,
     private authenticationService: AuthenticationService,
@@ -50,5 +55,39 @@ export class AddPropertyForm implements OnInit {
       this.router.navigate(["/login"]);
     }
   }
-  ngOnInit() {}
+  ngOnInit() {
+    this.addPropertyForm = this.formBuilder.group({
+      furnishDetails: [""],
+      titleDeedNumber: [""],
+      ProjectName: [""],
+      CommunityName: [""],
+      BuildingName: [""],
+      SizeArea: [""],
+      NumberOfBedRooms: [0],
+      UnitNumber: [0],
+      NumberOfCarParking: [0],
+      AdditionalInformation: [""],
+      SocialMediaMarketingDetails: [""],
+      BoardMarketingDetails: [""],
+      OthersMarketingDetails: [""],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.addPropertyForm.value["furnishDetails"]);
+  }
+
+  pickImg(event: any) {}
+
+  pickDoc(event: any) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    // reader.readAsDataURL(file);
+
+    // reader.onloadend = (e) => {
+    //   console.log(e.target.result);
+    // };
+
+    this.uploadedDocName = file["name"];
+  }
 }
