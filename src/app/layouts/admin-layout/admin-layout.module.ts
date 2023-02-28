7107655b9ba6a3e766cf6e3e672d0cce9d1afea6
indusRealEstate <webtech@indusre.ae>
2023-02-28 18:15:49 +0400

    refactor: :rocket: Optimized code by reducing imports in module.

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -14,40 +14,40 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { MatNativeDateModule, MatRippleModule } from "@angular/material/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatTooltipModule } from "@angular/material/tooltip";
+// import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatSelectModule } from "@angular/material/select";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatCardModule } from "@angular/material/card";
-import { A11yModule } from "@angular/cdk/a11y";
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { PortalModule } from "@angular/cdk/portal";
+// import { A11yModule } from "@angular/cdk/a11y";
+// import { DragDropModule } from "@angular/cdk/drag-drop";
+// import { PortalModule } from "@angular/cdk/portal";
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { CdkStepperModule } from "@angular/cdk/stepper";
 import { CdkTableModule } from "@angular/cdk/table";
-import { CdkTreeModule } from "@angular/cdk/tree";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatBadgeModule } from "@angular/material/badge";
-import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
+// import { CdkTreeModule } from "@angular/cdk/tree";
+// import { MatAutocompleteModule } from "@angular/material/autocomplete";
+// import { MatBadgeModule } from "@angular/material/badge";
+// import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+// import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatChipsModule } from "@angular/material/chips";
+// import { MatChipsModule } from "@angular/material/chips";
 import { MatStepperModule } from "@angular/material/stepper";
 import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatGridListModule } from "@angular/material/grid-list";
+// import { MatDividerModule } from "@angular/material/divider";
+// import { MatExpansionModule } from "@angular/material/expansion";
+// import { MatGridListModule } from "@angular/material/grid-list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
-import { MatMenuModule } from "@angular/material/menu";
+// import { MatMenuModule } from "@angular/material/menu";
 import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
+// import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatRadioModule } from "@angular/material/radio";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatSortModule } from "@angular/material/sort";
+// import { MatSidenavModule } from "@angular/material/sidenav";
+// import { MatSliderModule } from "@angular/material/slider";
+// import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+// import { MatSnackBarModule } from "@angular/material/snack-bar";
+// import { MatSortModule } from "@angular/material/sort";
 import { MatTableModule } from "@angular/material/table";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -107,7 +107,7 @@ import { AddPropertyForm } from "app/add-property-form/add_property_from";
 import { NgxCheckboxModule } from "ngx-checkbox";
 import { AdminRequests } from "app/admin-requests/admin-requests";
 import { PropertyPage } from "app/property-page/property-page.component";
-import { GoogleMapsModule } from "@angular/google-maps";
+// import { GoogleMapsModule } from "@angular/google-maps";
 import { NgApexchartsModule } from "ng-apexcharts";
 import { AdminDashboardComponent } from "app/admin_dashboard/admin_dashboard";
 import { AcceptRequestConfirmDialog } from "app/admin-requests/accept_req_dialog/acspt_req_dialog";
@@ -140,48 +140,48 @@ import { CountryDropdown } from "app/components/country-dropdown/country-dropdow
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatTooltipModule,
+    // MatTooltipModule,
     MatDialogModule,
     MatCardModule,
-    A11yModule,
-    DragDropModule,
-    PortalModule,
+    // A11yModule,
+    // DragDropModule,
+    // PortalModule,
     ScrollingModule,
     CdkStepperModule,
     CdkTableModule,
-    CdkTreeModule,
-    MatAutocompleteModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
-    MatButtonToggleModule,
+    // CdkTreeModule,
+    // MatAutocompleteModule,
+    // MatBadgeModule,
+    // MatBottomSheetModule,
+    // MatButtonToggleModule,
     MatCheckboxModule,
-    MatChipsModule,
+    // MatChipsModule,
     MatStepperModule,
     MatDatepickerModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
+    // MatDividerModule,
+    // MatExpansionModule,
+    // MatGridListModule,
     MatIconModule,
     MatListModule,
-    MatMenuModule,
+    // MatMenuModule,
     MatPaginatorModule,
-    MatProgressBarModule,
+    // MatProgressBarModule,
     MatProgressSpinnerModule,
     MatRadioModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
+    // MatSidenavModule,
+    // MatSliderModule,
+    // MatSlideToggleModule,
+    // MatSnackBarModule,
+    // MatSortModule,
     MatTableModule,
     MatTabsModule,
-    MatToolbarModule,
-    MatTreeModule,
+    // MatToolbarModule,
+    // MatTreeModule,
     MatNativeDateModule,
     FullCalendarModule,
     NgxSkeletonLoaderModule,
     NgxCheckboxModule,
-    GoogleMapsModule,
+    // GoogleMapsModule,
     NgApexchartsModule,
     // NgbModalModule,
     // FlatpickrModule.forRoot(),
