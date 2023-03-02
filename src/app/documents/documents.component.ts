798a2eac06ab541f3a13f1ca6e70ca5a915e3464
indusRealEstate <webtech@indusre.ae>
2023-03-02 18:01:56 +0400

    refactor: :zap: documents refactored.

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ApiService } from "app/services/api.service";
-import { MatTableDataSource } from "@angular/material/table";
+// import { MatTableDataSource } from "@angular/material/table";
 import { ActivatedRoute, Router } from "@angular/router";
+import { ApiService } from "app/services/api.service";
 import { AuthenticationService } from "app/services/authentication.service";
 
 @Component({
@@ -10,11 +10,35 @@ import { AuthenticationService } from "app/services/authentication.service";
   styleUrls: ["./documents.component.scss"],
 })
 export class DocumentsComponent implements OnInit {
-  selectedEntry: any = "10";
+  selectedSearchType: any;
 
-  dataSource = new MatTableDataSource<any>();
+  searchText: any;
+
+  searchResult: any[] = [];
 
   isUserSignedIn: boolean = false;
+
+  isSearchTypeNotSelected: boolean = false;
+  isSearchTextEmpty: boolean = false;
+
+  isSearchBtnClicked: boolean = false;
+
+  isLoading: boolean = false;
+
+  displayedColumns: string[] = [
+    // "name",
+    "docName",
+    "uploadDate",
+    "docSize",
+    "links",
+  ];
+
+  dataSource: any[] = [];
+
+  searchType: any[] = [
+    "Document Name",
+    // "Request type",
+  ];
 
   constructor(
     private apiService: ApiService,
@@ -22,6 +46,7 @@ export class DocumentsComponent implements OnInit {
     private authenticationService: AuthenticationService,
     private route: ActivatedRoute
   ) {
+    this.isLoading = true;
     var userData = localStorage.getItem("currentUser");
     var user = JSON.parse(userData);
 
@@ -38,6 +63,40 @@ export class DocumentsComponent implements OnInit {
     });
   }
 
+  searchRequest() {
+    if (this.selectedSearchType == null) {
+      this.isSearchTypeNotSelected = true;
+
+      setTimeout(() => {
+        this.isSearchTypeNotSelected = false;
+      }, 2500);
+    } else if (this.searchText == null || this.searchText == "") {
+      this.isSearchTextEmpty = true;
+
+      setTimeout(() => {
+        this.isSearchTextEmpty = false;
+      }, 2500);
+    } else {
+      this.isSearchBtnClicked = true;
+      if (this.selectedSearchType == "Document Name") {
+        this.searchResult.length = 0;
+        this.dataSource.map((val) => {
+          if (
+            new String(val.document_name).trim().toLowerCase() ==
+            new String(this.searchText).trim().toLowerCase()
+          ) {
+            return this.searchResult.push(val);
+          }
+        });
+      }
+    }
+  }
+
+  closeResults() {
+    this.searchResult.length = 0;
+    this.isSearchBtnClicked = false;
+  }
+
   isUserSignOut() {
     if (this.authenticationService.currentUserValue) {
       this.isUserSignedIn = true;
@@ -47,28 +106,49 @@ export class DocumentsComponent implements OnInit {
     }
   }
 
-  displayedColumns: string[] = [
-    "document-name",
-    "date",
-    "size",
-    "extension",
-    "view",
-  ];
-
   ngOnInit() {
     this.isUserSignOut();
-    this.getUserDocuments();
+
+    var sessionData = JSON.parse(sessionStorage.getItem("my-docs-session"));
+
+    if (sessionData != null) {
+      this.dataSource = sessionData["data"];
+      this.isLoading = false;
+    } else {
+      this.getUserRequestDetails();
+    }
   }
 
-  getUserDocuments() {
+  getUserRequestDetails() {
     var data = localStorage.getItem("currentUser");
     var user = JSON.parse(data);
     var userId = user[0]["id"];
 
-    this.apiService.getUserDocuments(userId).subscribe((data: any) => {
-      // console.log(JSON.stringify(data));
-      // console.log(data);
-      this.dataSource.data = data;
+    this.apiService.getUserDocuments(userId).subscribe((data: any[]) => {
+      this.dataSource = data;
     });
+    setTimeout(() => {
+      this.isLoading = false;
+      if (this.dataSource.length != 0) {
+        sessionStorage.setItem(
+          "my-docs-session",
+          JSON.stringify({
+            data: this.dataSource,
+          })
+        );
+      }
+    }, 3000);
+
+    // setTimeout(() => {
+    //   if(this.isLoading == false){
+    //     if(this.dataSource.)
+    //   }
+    // }, 3500);
   }
+
+  // navigateToRequestPage(req_no) {
+  //   this.router.navigate(["/request-page"], {
+  //     queryParams: { "req-no": req_no },
+  //   });
+  // }
 }
