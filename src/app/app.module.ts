cf2a2f82cec9ef48efeb953880ae010590e23407
indusRealEstate <webtech@indusre.ae>
2023-08-09 18:16:17 +0400

    fix: :zap: fixed issues with firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import {
-  connectFirestoreEmulator,
-  provideFirestore,
-} from "@angular/fire/firestore";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatDialogModule } from "@angular/material/dialog";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
 import { ChartsModule } from "@progress/kendo-angular-charts";
 import { IconsModule } from "@progress/kendo-angular-icons";
-import { environment } from "environments/environment";
-import { getFirestore } from "firebase/firestore";
 import "hammerjs";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing";
 import { ComponentsModule } from "./components/components.module";
 import { AdminLayoutComponent } from "./layouts/admin-layout.component";
-
-import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getApp } from "firebase/app";
-
-// import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 @NgModule({
   imports: [
@@ -36,13 +24,8 @@ import { getApp } from "firebase/app";
     MatDialogModule,
     ChartsModule,
     IconsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
   ],
   declarations: [AppComponent, AdminLayoutComponent],
-
-  // providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
